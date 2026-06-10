3422bbcf3101c3c50c8817f3abb0222c526f2e50
shuk.liu <biz.shuk@gmail.com>
2026-06-10 19:43:03 +0800

    feat: configure logging paths and environment variables for agentmemory in ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,16 @@ module.exports = {
     apps: [
         {
             name: "Agent Memory",
-            script: "agentmemory"
+            script: "agentmemory",
+            error_file: "~/.config/agentmemory/daemon.err",
+            out_file: "~/.config/agentmemory/daemon.out",
+            autorestart: true,
+            env: {
+                NODE_ENV: "development"
+            },
+            env_production: {
+                NODE_ENV: "production"
+            }
         }
     ]
 };
